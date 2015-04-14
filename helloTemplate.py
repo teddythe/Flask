8d8773df10a2db2258177bdb72958f25072ffca6
teddythe <teddy1993@mac.com>
2015-04-14 07:30:42 +0000

    Bootstrap Works!

--- helloTemplate.py
+++ helloTemplate.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
-from flask.ext.bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+Bootstrap(app)
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('user.html')
 
-@app.route('/user/<name>')
-def user(name):
-  return render_template('user.html', name=name)
+#@app.route('/user/<name>')
+#def user(name):
+#  return render_template('user.html', name=name)
 
 if __name__ == '__main__':
   app.run(debug = True, host = "0.0.0.0", port = 80)
