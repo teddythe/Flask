66d60510e198a97c38f149e80baedfc678a67660
teddythe <teddy1993@me.com>
2015-04-14 08:11:10 +0000

    404.html & 500.html added implements base.html

--- helloTemplate.py
+++ helloTemplate.py
@@ -6,6 +6,13 @@ Bootstrap(app)
 @app.route('/')
 def index():
   return render_template('user.html')
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.errorhandler(505)
+def internal_server_error(e):
+	return render_template('500.html'), 500
 
 #@app.route('/user/<name>')
 #def user(name):
