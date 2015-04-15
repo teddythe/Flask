2de0b1b1a7b94d30501d82102590cc35bd0becac
teddythe <teddy1993@me.com>
2015-04-15 03:10:00 +0000

    Date and time added!

--- helloTemplate.py
+++ helloTemplate.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask.ext.moment import Moment
 
 app = Flask(__name__)
 Bootstrap(app)
+Moment(app)
 @app.route('/')
 def index():
-  return render_template('user.html')
+  return render_template('index.html', current_time=datetime.utcnow())
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'), 404
